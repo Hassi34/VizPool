9660b8ab9b1f1272e7fb8465d267d1b1ff832e46
hassi34 <hasnainmehmood3435@gmail.com>
2022-10-10 17:20:39 +0700

    feature imp

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USERNAME = "Hassi34"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}",
-    version="0.0.9.dev4",
+    version="0.0.9.dev5",
     author= USERNAME,
     author_email="hasanain@aicaliber.com",
     description="A Python Library with Low-Code support for Basic to Advance Static & Interactive Visualizations",
