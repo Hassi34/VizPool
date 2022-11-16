eae8256659beec8f9120dd3a23c6aca300f589a8
hassi34 <hasnainmehmood3435@gmail.com>
2022-11-16 10:59:08 +0700

    bar_line

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ USERNAME = "Hassi34"
 setuptools.setup(
     name=f"{PROJECT_NAME}",
     include_package_data = True,
-    version="0.0.11.dev3",
+    version="0.0.11.dev4",
     license='MIT',
     author= "Hasanain Mehmood",
     author_email="hasanain@aicaliber.com",
